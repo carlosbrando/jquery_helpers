d19a572d2c6ad8cf5fbc92d39a126b2faacb25b1
Carlos Brando <eduardobrando@gmail.com>
2009-01-22 18:05:11 -0200

    Refactoring defaultValueActsAsHint.

--- defaultValueActsAsHint/jquery.defaultvalueactsashint.js
+++ defaultValueActsAsHint/jquery.defaultvalueactsashint.js
@@ -1,18 +1,17 @@
 (function($){ 
 	$.fn.defaultValueActsAsHint = function() {
 		return this.each(function() {
-			element = $(this);
-			element.attr('_default', element.attr('value'))
-			element.addClass('hint');
+			$(this).attr('_default', $(this).attr('value'))
+			$(this).addClass('hint');
 
-			element.bind('focus', function(event) {
-				if ($(this).attr('_default') != $(this).attr('value')) return;
-				$(this).removeClass('hint').attr('value', '');
+			$(this).bind('focus', function(event) {
+				if ($(this).attr('_default') == $(this).attr('value'))
+					$(this).removeClass('hint').attr('value', '');
 			});
 
-			element.blur(function() {
-				if ($.trim($(this).attr('value')) != '') return;
-				$(this).addClass('hint').attr('value', $(this).attr('_default'));
+			$(this).blur(function() {
+				if ($.trim($(this).attr('value')) != '')
+					$(this).addClass('hint').attr('value', $(this).attr('_default'));
 			});
 
 		});
